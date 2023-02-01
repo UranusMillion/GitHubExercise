e11763b3efbf9493b55aa50599cb5cf2380f2c81
Leon <rock_sounding@hotmail.com>
2023-02-01 13:55:47 +0800

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by leonlee on 2023/2/1.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
